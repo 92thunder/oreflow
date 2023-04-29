fa7dd997281cd6783775a2c26c43502b7d5fc6d4
ryota.kunisada <r.kunisada661@gmail.com>
2023-04-29 22:51:46 +0900

    tab layout

--- apps/web/src/components/Main.tsx
+++ apps/web/src/components/Main.tsx
@@ -1,12 +1,29 @@
-import { Box, Container, Input, VStack } from "@chakra-ui/react"
+import { Box, Card, CardBody, Checkbox, Container, HStack, Input, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack } from "@chakra-ui/react"
 
 export const Main = () => {
 	return (
 		<Box py={4} bg="gray.50" as="main" flexGrow="1">
 			<Container maxW="container.xl">
-				<VStack>
-					<Input placeholder="+" bg="white" />
-				</VStack>
+				<Tabs>
+					<TabList>
+						<Tab>ALL</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel px="0">
+							<VStack>
+								<Input placeholder="+" bg="white" />
+								<Card width="full" size="sm">
+									<CardBody>
+										<HStack>
+											<Checkbox />
+											<Text>test</Text>
+										</HStack>
+									</CardBody>
+								</Card>
+							</VStack>
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
 			</Container>
 		</Box>
 	)
